97f6ae9253e462c2feac9dd924af0a48f3888a45
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 01:23:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,17 +2,17 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
-  name_array.collect do |name|
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
      badge_maker(name)
   end
 end
 
-def assign_rooms(speaker_list)
+def assign_rooms(attendees)
 
   room_assignments = Array.new
 
-  speaker_list.each_with_index do |speaker, index|
+  attendees.each_with_index do |speaker, index|
     room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 
