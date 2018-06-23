d03bfd4cbf7057f7814bfb3b32b9fe72fd0f39d7
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 01:17:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name_array)
-  name_array.each do |name|
-    badge_maker(name)
+
+
+  name_array.collect do |name|
+     badge_maker(name)
   end
 end
